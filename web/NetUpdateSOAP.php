615dbf2905e301ee556bfe0c60da2d0947353b11
Thiago Oliveira <tgt@certi.org.br>
2016-11-08 13:50:52 -0200

    removed ninupdate stuff / fixed config file

--- web/NetUpdateSOAP.php
+++ web/NetUpdateSOAP.php
@@ -1,135 +1,76 @@
 <?php
 
-include_once("/home/yellows8/ninupdates/api.php");
-
 function initialize()
 {
-	global $mysqldb, $hdrs, $soapreq, $fp, $system, $region, $sitecfg_workdir, $soapreq_data;
-
-	$soapreq_data = file_get_contents("php://input");
-
-	$hdrs = array('SOAPAction: "urn:nus.wsapi.broadon.com/GetSystemUpdate"', 'Content-Type: application/xml', 'Content-Size: '.strlen($soapreq_data));
+    global $hdrs, $soapreq_data;
+    $soapreq_data = file_get_contents("php://input");
+    $hdrs = ['SOAPAction: "urn:nus.wsapi.broadon.com/GetSystemUpdate"', 'Content-Type: application/xml', 'Content-Size: ' . strlen($soapreq_data)];
 }
 
 function init_curl()
 {
-	global $curl_handle, $sitecfg_workdir, $error_FH;
-
-	$error_FH = fopen("$sitecfg_workdir/debuglogs/ctr-httpwn_web_NetUpdateSOAP_error.log","w");
-	$curl_handle = curl_init();
+    global $curl_handle;
+    $curl_handle = curl_init();
 }
 
 function close_curl()
 {
-	global $curl_handle, $error_FH;
-
-	curl_close($curl_handle);
-	fclose($error_FH);
+    global $curl_handle;
+    curl_close($curl_handle);
 }
 
 function send_httprequest($url)
 {
-	global $mysqldb, $hdrs, $soapreq, $httpstat, $sitecfg_workdir, $soapreq_data, $curl_handle, $system, $error_FH;
+    global $hdrs, $httpstat, $soapreq_data, $curl_handle;
 
-	$query="SELECT clientcertfn, clientprivfn FROM ninupdates_consoles WHERE system='".$system."'";
-	$result=mysqli_query($mysqldb, $query);
-	$row = mysqli_fetch_row($result);
-	$clientcertfn = $row[0];
-	$clientprivfn = $row[1];
+    curl_setopt($curl_handle, CURLOPT_VERBOSE, true);
+    curl_setopt($curl_handle, CURLOPT_USERAGENT, "CTR EC 040600 Mar 14 2012 13:32:39");
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $hdrs);
+    curl_setopt($curl_handle, CURLOPT_POST, 1);
+    curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $soapreq_data);
+    curl_setopt($curl_handle, CURLOPT_URL, $url);
 
-	curl_setopt($curl_handle, CURLOPT_VERBOSE, true);
-	curl_setopt ($curl_handle, CURLOPT_STDERR, $error_FH );
+    $buf = curl_exec($curl_handle);
+    $errorstr = "";
+    $httpstat = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
 
-	curl_setopt($curl_handle, CURLOPT_USERAGENT, "CTR EC 040600 Mar 14 2012 13:32:39");
+    if ($buf === false) {
+        $errorstr = "HTTP request failed: " . curl_error($curl_handle);
+        $httpstat = "0";
+    } else {
+        if ($httpstat != "200") {
+            $errorstr = "HTTP error $httpstat: " . curl_error($curl_handle);
+        }
+    }
 
-	curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+    if ($errorstr != "") {
+        $buf = $errorstr;
+    }
 
-	curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $hdrs);
-
-	curl_setopt($curl_handle, CURLOPT_POST, 1);
-	curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $soapreq_data);
-
-	curl_setopt($curl_handle, CURLOPT_URL, $url);
-
-	if(strstr($url, "https") && $clientcertfn!="" && $clientprivfn!="")
-	{
-		curl_setopt($curl_handle, CURLOPT_SSLCERTTYPE, "PEM");
-		curl_setopt($curl_handle, CURLOPT_SSLCERT, "$sitecfg_workdir/sslcerts/$clientcertfn");
-		curl_setopt($curl_handle, CURLOPT_SSLKEY, "$sitecfg_workdir/sslcerts/$clientprivfn");
-
-		curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, 0);
-	}
-
-	$buf = curl_exec($curl_handle);
-
-	$errorstr = "";
-
-	$httpstat = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
-	if($buf===FALSE)
-	{
-		$errorstr = "HTTP request failed: " . curl_error ($curl_handle);
-		$httpstat = "0";
-	} else if($httpstat!="200")$errorstr = "HTTP error $httpstat: " . curl_error ($curl_handle);
-
-	if($errorstr!="")$buf = $errorstr;
-
-	return $buf;
+    return $buf;
 }
 
 $SOAPAction = $_SERVER['HTTP_SOAPACTION'];
 $SOAPAction_prefix = "urn:nus.wsapi.broadon.com/";
-
 $reply_TitleHash = "00000000000000000000000000000000";
-
-$soapdata_filepath = "$sitecfg_workdir/ctr-httpwn/NetUpdateSOAP_soapreplydata";
 $soapdata = "";
-if(file_exists($soapdata_filepath))$soapdata = file_get_contents($soapdata_filepath);
 
-if($soapdata!=="")
-{
-	$titlehash_pos = strpos($soapdata, "<TitleHash>") + 11;
-	$titlehash_posend = strpos($soapdata, "</TitleHash>");
-	if($titlehash_pos!==FALSE && $titlehash_posend!==FALSE)
-	{
-		$reply_TitleHash = substr($soapdata, $titlehash_pos, $titlehash_posend - $titlehash_pos);
-	}
+if ($SOAPAction === $SOAPAction_prefix . "GetSystemTitleHash") {
+    $GetSystemTitleHashResponse = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body><GetSystemTitleHashResponse xmlns=\"urn:nus.wsapi.broadon.com\"><Version>1.0</Version><DeviceId></DeviceId><MessageId></MessageId><TimeStamp></TimeStamp><ErrorCode>0</ErrorCode><TitleHash>$reply_TitleHash</TitleHash></GetSystemTitleHashResponse></soapenv:Body></soapenv:Envelope>";
+    echo $GetSystemTitleHashResponse;
+} elseif ($SOAPAction === $SOAPAction_prefix . "GetSystemUpdate") {
+    $GetSystemUpdateResponse = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body><GetSystemUpdateResponse xmlns=\"urn:nus.wsapi.broadon.com\"><Version>1.0</Version><DeviceId></DeviceId><MessageId>1</MessageId><TimeStamp></TimeStamp><ErrorCode>0</ErrorCode><ContentPrefixURL>http://nus.cdn.c.shop.nintendowifi.net/ccs/download</ContentPrefixURL><UncachedContentPrefixURL>https://ccs.c.shop.nintendowifi.net/ccs/download</UncachedContentPrefixURL><TitleVersion><TitleId>0004003000008A02</TitleId><Version>0</Version><FsSize>262144</FsSize><TicketSize>848</TicketSize><TMDSize>4660</TMDSize></TitleVersion><UploadAuditData>1</UploadAuditData><TitleHash>$reply_TitleHash</TitleHash></GetSystemUpdateResponse></soapenv:Body></soapenv:Envelope>";
+    if ($soapdata !== "") {
+        $GetSystemUpdateResponse = $soapdata;
+    }
+    echo $GetSystemUpdateResponse;
+} elseif ($SOAPAction === $SOAPAction_prefix . "GetSystemCommonETicket") {
+    init_curl();
+    initialize();
+    $con = send_httprequest("https://nus.c.shop.nintendowifi.net/nus/services/NetUpdateSOAP");
+    close_curl();
+    echo $con;
+} else {
+    echo "Unrecognized SOAPAction header, it probably wasn't set correctly.";
 }
-
-if($SOAPAction === $SOAPAction_prefix . "GetSystemTitleHash")
-{
-	$GetSystemTitleHashResponse = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body><GetSystemTitleHashResponse xmlns=\"urn:nus.wsapi.broadon.com\"><Version>1.0</Version><DeviceId></DeviceId><MessageId></MessageId><TimeStamp></TimeStamp><ErrorCode>0</ErrorCode><TitleHash>$reply_TitleHash</TitleHash></GetSystemTitleHashResponse></soapenv:Body></soapenv:Envelope>";
-
-	echo $GetSystemTitleHashResponse;
-}
-else if($SOAPAction === $SOAPAction_prefix . "GetSystemUpdate")
-{
-	$GetSystemUpdateResponse = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><soapenv:Body><GetSystemUpdateResponse xmlns=\"urn:nus.wsapi.broadon.com\"><Version>1.0</Version><DeviceId></DeviceId><MessageId>1</MessageId><TimeStamp></TimeStamp><ErrorCode>0</ErrorCode><ContentPrefixURL>http://nus.cdn.c.shop.nintendowifi.net/ccs/download</ContentPrefixURL><UncachedContentPrefixURL>https://ccs.c.shop.nintendowifi.net/ccs/download</UncachedContentPrefixURL><TitleVersion><TitleId>0004003000008A02</TitleId><Version>0</Version><FsSize>262144</FsSize><TicketSize>848</TicketSize><TMDSize>4660</TMDSize></TitleVersion><UploadAuditData>1</UploadAuditData><TitleHash>$reply_TitleHash</TitleHash></GetSystemUpdateResponse></soapenv:Body></soapenv:Envelope>";
-
-	if($soapdata!=="")$GetSystemUpdateResponse = $soapdata;
-
-	echo $GetSystemUpdateResponse;
-}
-else if($SOAPAction === $SOAPAction_prefix . "GetSystemCommonETicket")
-{
-	$system = "ctr";
-
-	dbconnection_start();
-
-	db_checkmaintenance(1);
-
-	init_curl();
-	initialize();
-	$con = send_httprequest("https://nus.c.shop.nintendowifi.net/nus/services/NetUpdateSOAP");
-	close_curl();
-
-	dbconnection_end();
-
-	echo $con;
-}
-else
-{
-	echo "Unrecognized SOAPAction header, it probably wasn't set correctly.";
-}
-
-?>
--- web/config.php
+++ web/config.php
@@ -1,170 +1,299 @@
 <?php
 
-include_once("/home/yellows8/ninupdates/api.php");
-
-$retval = ninupdates_api("gettitleversions", "ctr", "E", "000400DB00016302", 2);
-if($retval!=0)
-{
-	writeNormalLog("ctr-httpwn config.php: API returned error $retval, for NVer. RESULT: 500");
-	header('HTTP/1.1 500 Internal Server Error');
-	die("A server-side error occured.\n");
+if (isset($_SERVER['HTTPS']) && (strcasecmp($_SERVER['HTTPS'], 'on') === 0 || $_SERVER['HTTPS'] == 1) ||
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strcasecmp($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') === 0
+) {
+    $http = 'https';
+} else {
+    $http = 'http';
 }
 
-$NVer_hextitlever = sprintf("%04X", substr($ninupdatesapi_out_version_array[0], 1));
-
-$retval = ninupdates_api("gettitleversions", "ctr", "E", "0004013000003202", 2);
-if($retval!=0)
-{
-	writeNormalLog("ctr-httpwn config.php: API returned error $retval, for friends-module. RESULT: 500");
-	header('HTTP/1.1 500 Internal Server Error');
-	die("A server-side error occured.\n");
-}
-
-$fpdver = file_get_contents("$sitecfg_workdir/friendsmodule_fpdvers/" . $ninupdatesapi_out_version_array[0]);
-if($fpdver===FALSE || $fpdver==="")
-{
-	writeNormalLog("ctr-httpwn config.php: Failed to load a valid fpdver file. RESULT: 500");
-	header('HTTP/1.1 500 Internal Server Error');
-	die("The config xml is currently not available, probably due to a new sysupdate just being released. It will automatically become available once ninupdates sysupdate post-processing finishes.\n");
-}
-
-//Base64-encode fpdver, with '=' switched to '*'.
-$fpdverbase64 = base64_encode($fpdver);
-while(strpos($fpdverbase64, "=")!==FALSE)$fpdverbase64 = str_replace("=", "*", $fpdverbase64);
-
-$targets_array = array();
-$targets_verarray = array();
-
-$targets_array[] = "tiger,eShop app,0004001000020900,JPN,J";
-$targets_array[] = "tiger,eShop app,0004001000021900,USA,E";
-$targets_array[] = "tiger,eShop app,0004001000022900,EUR,P";
-$targets_array[] = "mint,mint,000400300000C602,JPN,J";
-$targets_array[] = "mint,mint,000400300000CE02,USA,E";
-$targets_array[] = "mint,mint,000400300000D602,EUR,P";
-$targets_array[] = "mset,System Settings,0004001000020000,JPN,J";
-$targets_array[] = "mset,System Settings,0004001000021000,USA,E";
-$targets_array[] = "mset,System Settings,0004001000022000,EUR,P";
-$targets_array[] = "cardboar,System Transfer,0004001000020A00,JPN,J";
-$targets_array[] = "cardboar,System Transfer,0004001000021A00,USA,E";
-$targets_array[] = "cardboar,System Transfer,0004001000022A00,EUR,P";
-$targets_array[] = "nnid_settings,NNID Settings,000400100002BF00,JPN,J";
-$targets_array[] = "nnid_settings,NNID Settings,000400100002C000,USA,E";
-$targets_array[] = "nnid_settings,NNID Settings,000400100002C100,EUR,P";
-
-$targets_array_size = count($targets_array);
-
-for($i=0; $i<$targets_array_size; $i++)
-{
-	$titlename = strtok($targets_array[$i], ",");
-	$printname = strtok(",");
-	$titleid = strtok(",");
-	$region = strtok(",");
-	$regid = strtok(",");
-
-	$retval = ninupdates_api("gettitleversions", "ctr", $regid, $titleid, 2);
-	if($retval!=0)
-	{
-		writeNormalLog("ctr-httpwn config.php: API returned error $retval, for target $i. RESULT: 500");
-		header('HTTP/1.1 500 Internal Server Error');
-		die("A server-side error occured.\n");
-	}
-
-	$remaster_version = file_get_contents("$sitecfg_workdir/remaster_versions/$titlename/$region/" . $ninupdatesapi_out_version_array[0]);
-	if($remaster_version===FALSE || $remaster_version==="")
-	{
-		writeNormalLog("ctr-httpwn config.php: Failed to load a valid remaster_version file, for target $i. RESULT: 500");
-		header('HTTP/1.1 500 Internal Server Error');
-		die("The config xml is currently not available, probably due to a new sysupdate just being released. It will automatically become available once ninupdates sysupdate post-processing finishes.\n");
-	}
-
-	$targets_verarray[] = $remaster_version;
-}
-
-$reqoverrides = "";
-$reqoverrides_baseid = 16;
-
-for($i=0; $i<$targets_array_size; $i++)
-{
-	$titlename = strtok($targets_array[$i], ",");
-	$printname = strtok(",");
-	$titleid = strtok(",");
-	$region = strtok(",");
-	$regid = strtok(",");
-	$curid = $reqoverrides_baseid + $i;
-
-	$reqoverrides.="
-	<!-- $region $printname titleID. -->
-	<requestoverride type=\"reqheader\">
-		<id>$curid</id>
-		<setid_onmatch>1</setid_onmatch>
-		<name>X-Nintendo-Title-ID</name>
-		<value>$titleid</value>
-	</requestoverride>\n";
-}
-
-for($i=0; $i<$targets_array_size; $i++)
-{
-	$titlename = strtok($targets_array[$i], ",");
-	$printname = strtok(",");
-	$titleid = strtok(",");
-	$region = strtok(",");
-	$regid = strtok(",");
-
-	$reqid = $reqoverrides_baseid + $i;
-	$curid = $reqid + $targets_array_size;
-
-	$remaster_version = $targets_verarray[$i];
-
-	$reqoverrides.="
-	<!-- Remaster version override for $region $printname. -->
-	<requestoverride type=\"reqheader\">
-		<id>$curid</id>
-		<required_id>$reqid</required_id>
-		<name>X-Nintendo-Application-Version</name>
-		<new_value>$remaster_version</new_value>
-	</requestoverride>\n";
-}
+$curUrl = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$curUrl = str_replace('config.php', 'NetUpdateSOAP.php', $curUrl);
+$newUrl = "$http://$curUrl";
 ?>
+
 <?xml version="1.0" encoding="UTF-8"?>
 <config>
 
-	<incompatsysver_message>The sysmodule version must be the one from system-version >=9.6.0-X.</incompatsysver_message>
+    <incompatsysver_message>The sysmodule version must be the one from system-version >=9.6.0-X.
+    </incompatsysver_message>
 
-	<targeturl> <!-- This is the URL used for doing the actual sysupdate check / getting the the list of sysupdate titles. -->
-		<name>NetUpdateSOAP</name>
-		<caps>SendPOSTDataRawTimeout</caps>
-		<url>https://nus.c.shop.nintendowifi.net/nus/services/NetUpdateSOAP</url>
-		<new_url>https://yls8.mtheall.com/ctr-httpwn/NetUpdateSOAP.php</new_url>
-	</targeturl>
+    <targeturl>
+        <!-- This is the URL used for doing the actual sysupdate check / getting the the list of sysupdate titles. -->
+        <name>NetUpdateSOAP</name>
+        <caps>SendPOSTDataRawTimeout</caps>
+        <url>https://nus.c.shop.nintendowifi.net/nus/services/NetUpdateSOAP</url>
+        <new_url><?= $newUrl ?></new_url>
+    </targeturl>
 
-	<targeturl>
-		<name>nasc</name>
-		<caps>AddRequestHeader AddPostDataAscii</caps>
-		<url>https://nasc.nintendowifi.net/ac</url>
+    <targeturl>
+        <name>nasc</name>
+        <caps>AddRequestHeader AddPostDataAscii</caps>
+        <url>https://nasc.nintendowifi.net/ac</url>
 
-		<requestoverride type="reqheader">
-			<name>User-Agent</name>
-			<new_value>CTR FPD/<?=$fpdver?></new_value>
-		</requestoverride>
+        <requestoverride type="reqheader">
+            <name>User-Agent</name>
+            <new_value>CTR FPD/0008</new_value>
+        </requestoverride>
 
-		<requestoverride type="postform">
-			<name>fpdver</name>
-			<new_value><?=$fpdverbase64?></new_value>
-		</requestoverride>
-	</targeturl>
+        <requestoverride type="postform">
+            <name>fpdver</name>
+            <new_value>MDAwOA**</new_value>
+        </requestoverride>
+    </targeturl>
 
-	<targeturl> <!-- NNID -->
-		<name>NNID</name>
-		<caps>AddRequestHeader</caps>
-		<url>https://account.nintendo.net/</url>
+    <targeturl> <!-- NNID -->
+        <name>NNID</name>
+        <caps>AddRequestHeader</caps>
+        <url>https://account.nintendo.net/</url>
 
-		<requestoverride type="reqheader">
-			<id>3</id>
-			<name>X-Nintendo-System-Version</name>
-			<new_value><?=$NVer_hextitlever?></new_value>
-		</requestoverride>
+        <requestoverride type="reqheader">
+            <id>3</id>
+            <name>X-Nintendo-System-Version</name>
+            <new_value>0230</new_value>
+        </requestoverride>
 
-		<?=$reqoverrides?>
-	</targeturl>
+
+        <!-- JPN eShop app titleID. -->
+        <requestoverride type="reqheader">
+            <id>16</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>0004001000020900</value>
+        </requestoverride>
+
+        <!-- USA eShop app titleID. -->
+        <requestoverride type="reqheader">
+            <id>17</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>0004001000021900</value>
+        </requestoverride>
+
+        <!-- EUR eShop app titleID. -->
+        <requestoverride type="reqheader">
+            <id>18</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>0004001000022900</value>
+        </requestoverride>
+
+        <!-- JPN mint titleID. -->
+        <requestoverride type="reqheader">
+            <id>19</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>000400300000C602</value>
+        </requestoverride>
+
+        <!-- USA mint titleID. -->
+        <requestoverride type="reqheader">
+            <id>20</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>000400300000CE02</value>
+        </requestoverride>
+
+        <!-- EUR mint titleID. -->
+        <requestoverride type="reqheader">
+            <id>21</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>000400300000D602</value>
+        </requestoverride>
+
+        <!-- JPN System Settings titleID. -->
+        <requestoverride type="reqheader">
+            <id>22</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>0004001000020000</value>
+        </requestoverride>
+
+        <!-- USA System Settings titleID. -->
+        <requestoverride type="reqheader">
+            <id>23</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>0004001000021000</value>
+        </requestoverride>
+
+        <!-- EUR System Settings titleID. -->
+        <requestoverride type="reqheader">
+            <id>24</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>0004001000022000</value>
+        </requestoverride>
+
+        <!-- JPN System Transfer titleID. -->
+        <requestoverride type="reqheader">
+            <id>25</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>0004001000020A00</value>
+        </requestoverride>
+
+        <!-- USA System Transfer titleID. -->
+        <requestoverride type="reqheader">
+            <id>26</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>0004001000021A00</value>
+        </requestoverride>
+
+        <!-- EUR System Transfer titleID. -->
+        <requestoverride type="reqheader">
+            <id>27</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>0004001000022A00</value>
+        </requestoverride>
+
+        <!-- JPN NNID Settings titleID. -->
+        <requestoverride type="reqheader">
+            <id>28</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>000400100002BF00</value>
+        </requestoverride>
+
+        <!-- USA NNID Settings titleID. -->
+        <requestoverride type="reqheader">
+            <id>29</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>000400100002C000</value>
+        </requestoverride>
+
+        <!-- EUR NNID Settings titleID. -->
+        <requestoverride type="reqheader">
+            <id>30</id>
+            <setid_onmatch>1</setid_onmatch>
+            <name>X-Nintendo-Title-ID</name>
+            <value>000400100002C100</value>
+        </requestoverride>
+
+        <!-- Remaster version override for JPN eShop app. -->
+        <requestoverride type="reqheader">
+            <id>31</id>
+            <required_id>16</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0015</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for USA eShop app. -->
+        <requestoverride type="reqheader">
+            <id>32</id>
+            <required_id>17</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0015</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for EUR eShop app. -->
+        <requestoverride type="reqheader">
+            <id>33</id>
+            <required_id>18</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0016</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for JPN mint. -->
+        <requestoverride type="reqheader">
+            <id>34</id>
+            <required_id>19</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0010</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for USA mint. -->
+        <requestoverride type="reqheader">
+            <id>35</id>
+            <required_id>20</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0010</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for EUR mint. -->
+        <requestoverride type="reqheader">
+            <id>36</id>
+            <required_id>21</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0011</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for JPN System Settings. -->
+        <requestoverride type="reqheader">
+            <id>37</id>
+            <required_id>22</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>000A</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for USA System Settings. -->
+        <requestoverride type="reqheader">
+            <id>38</id>
+            <required_id>23</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0009</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for EUR System Settings. -->
+        <requestoverride type="reqheader">
+            <id>39</id>
+            <required_id>24</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0009</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for JPN System Transfer. -->
+        <requestoverride type="reqheader">
+            <id>40</id>
+            <required_id>25</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0006</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for USA System Transfer. -->
+        <requestoverride type="reqheader">
+            <id>41</id>
+            <required_id>26</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0006</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for EUR System Transfer. -->
+        <requestoverride type="reqheader">
+            <id>42</id>
+            <required_id>27</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0006</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for JPN NNID Settings. -->
+        <requestoverride type="reqheader">
+            <id>43</id>
+            <required_id>28</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0003</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for USA NNID Settings. -->
+        <requestoverride type="reqheader">
+            <id>44</id>
+            <required_id>29</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0003</new_value>
+        </requestoverride>
+
+        <!-- Remaster version override for EUR NNID Settings. -->
+        <requestoverride type="reqheader">
+            <id>45</id>
+            <required_id>30</required_id>
+            <name>X-Nintendo-Application-Version</name>
+            <new_value>0003</new_value>
+        </requestoverride>
+    </targeturl>
 </config>
 
